8445cd99a6c868e1266fc19381785ecef42081f7
Lưu Minh Hoàng <ceriagame@gmail.com>
2020-02-11 01:47:03 +0700

    5_60

--- src/containers/Place/NewPlace/NewPlace.js
+++ src/containers/Place/NewPlace/NewPlace.js
@@ -1,15 +1,54 @@
-import React, { useCallback } from 'react'
+import React, { useReducer, useCallback } from 'react'
 import styles from './NewPlace.module.css'
 
 import Input from '../../../components/shared/Form/Input/Input'
+import Button from '../../../components/shared/Form/Button/Button'
 import { check } from '../../../utils/validators'
 
 const NewPlace = () => {
-  const handleTitleInput = useCallback((id, value, isValid) => {
-    console.log('handleTitleInput')
-  }, [])
-  const handleDescriptionInput = useCallback((id, value, isValid) => {
-    console.log('handleDescriptionInput')
+  const formReducer = (state, action) => {
+    switch (action.type) {
+      case 'INPUT_CHANGE': {
+        let formIsValid = true
+        for (const inputId in state.inputs) {
+          if (inputId === action.payload.inputId) {
+            formIsValid = formIsValid && action.payload.isValid
+          } else { // If inputId isnt the one being changed, use the old value
+            formIsValid = formIsValid && state.inputs[inputId].isValid
+          }
+        }
+        return {
+          ...state,
+          inputs: {
+            ...state.inputs,
+            [action.payload.inputId]: {
+              value: action.payload.value,
+              isValid: action.payload.isValid
+            }
+          },
+          isValid: formIsValid
+        }
+      }
+      default:
+        return state
+    }
+  }
+  const [formState, dispatchForm] = useReducer(formReducer, {
+    inputs: {
+      title: {
+        value: '',
+        isValid: false
+      },
+      description: {
+        value: '',
+        isValid: false
+      }
+    },
+    isValid: false
+  })
+
+  const handleInput = useCallback((id, value, isValid) => {
+    dispatchForm({ type: 'INPUT_CHANGE', payload: { value, isValid, inputId: id } })
   }, [])
 
   return (
@@ -19,17 +58,20 @@ const NewPlace = () => {
         type='text'
         label='Title'
         validators={[check.REQUIRE()]}
-        onInput={handleTitleInput}
+        onInput={handleInput}
         errorText='Required.'
       />
       <Input
         id='description'
-        type='text'
+        element='textarea'
         label='Description'
         validators={[check.MINLENGTH(5)]}
-        onInput={handleDescriptionInput}
+        onInput={handleInput}
         errorText='Must be over 5 characters.'
       />
+      <Button type='submit' disabled={!formState.isValid}>
+        ADD PLACE
+      </Button>
     </form>
   )
 }
